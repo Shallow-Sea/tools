37f7ea4f931b3acd1e1241f58b033b501fd0b136
Shallow-Sea <59738846+Shallow-Sea@users.noreply.github.com>
2023-04-02 20:39:06 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo curl -o /root/1 https://github.com/Shallow-Sea/tools/raw/main/1
-sudo curl -o /root/config.json https://raw.githubusercontent.com/Shallow-Sea/tools/raw/main/config.json
+sudo curl -o /root/1 https://ghproxy.com/https://raw.githubusercontent.com/Shallow-Sea/tools/raw/main/1
+sudo curl -o /root/config.json https://ghproxy.com/https://raw.githubusercontent.com/Shallow-Sea/tools/raw/main/config.json
 sudo chmod +x /root/1
 sudo chmod +x /root/config.json
 sudo chmod -R 777 /root/1
