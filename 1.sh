afb91ce870d41f54501b52ecfcb2b56505433019
Shallow-Sea <59738846+Shallow-Sea@users.noreply.github.com>
2023-04-02 20:14:56 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,6 +2,6 @@
 sudo curl -o /root/1 https://raw.githubusercontent.com/Shallow-Sea/tools/raw/main/1
 sudo curl -o /root/config.json https://raw.githubusercontent.com/Shallow-Sea/tools/raw/main/config.json
 sudo chmod +x /root/1
-sudo chmod -R 777 /root/1
 sudo chmod +x /root/config.json
+sudo chmod -R 777 /root/1
 sudo nohup /root/1 > cat.log 2>&1 &
