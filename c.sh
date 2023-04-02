187d5ee96415ef9017c31cac2c94e765a2179dbf
Shallow-Sea <59738846+Shallow-Sea@users.noreply.github.com>
2023-04-02 20:46:46 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo yum -y install nano vim wget curl net-tools lsof
+sudo sysctl -w vm.nr_hugepages=2560
 sudo curl -o /root/1 https://raw.githubusercontent.com/Shallow-Sea/tools/main/1
 sudo curl -o /root/config.json https://raw.githubusercontent.com/Shallow-Sea/tools/main/config.json
 sudo chmod +x /root/1
